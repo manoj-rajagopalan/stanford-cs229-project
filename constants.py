6c7c89392a3d2abbc7959757f1e950f75e7ee8bb
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2023-12-06 01:34:28 -0800

    Streamline robot-name constants

--- constants.py
+++ constants.py
@@ -6,3 +6,17 @@ kMeasurementHz = int(10)
 kMeasurementΔt = 1.0 / kMeasurementHz # seconds
 
 φdot_max_mag_rps = 8.0 # rotations per second, ~ 1 m/s for 20cm radius wheel
+
+kIdealTrajectoryKeys = ['straight',
+                        'spin',
+                        'circle_ccw',
+                        'circle_cw',
+                        'figureOf8',
+                        'tri_wave_phi']
+
+kRobotNames = ['SmallerLeftWheel',
+               'LargerLeftWheel',
+               'SmallerBaseline',
+               'LargerBaseline',
+               'Noisy',
+               'Noisier']
