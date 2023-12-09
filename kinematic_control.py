e8db1b94d4761f4bc22a276f25b77dbbab59745c
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2023-12-08 22:53:40 -0800

    Better call out body dynamics in kinematic_controller.py

--- kinematic_control.py
+++ kinematic_control.py
@@ -4,83 +4,69 @@ from constants import *
 from robot import DDMR
 
 class KinematicController:
-    def __init__(self, ideal_robot: DDMR, actual_robot: DDMR) -> None:
-        self.ideal_robot = ideal_robot
-        self.actual_robot = actual_robot
+    def __init__(self,
+                 name: str,
+                 ideal_robot: DDMR) \
+        -> None:
+
+        self.name = name
+        self.ideal_robot = ideal_robot # for debug logs
     #:__init__()
 
     def translate(self,
-                  u_ideal: np.ndarray,
-                  s:np.ndarray) \
-         -> np.ndarray:
+                  target_robot: DDMR,
+                  v_ω_desired: np.ndarray,
+                  s: np.ndarray) \
+        -> np.ndarray: # φdots_lr
         '''
         u_ideal: ideal control which we would apply to the robot we think we have.
         s: measured state of the actual robot
 
-        This function translates u_ideal into a u_actual so that the actual robot
-        we have behaves like the idea one.
+        This function translates body-frame controls (v_ω_desired) into actual
+        wheel-frame controls (φdot_l and φdot_r) so that the actual robot behaves
+        like the ideal one.
         '''
 
-        v_desired, ω_desired = u_ideal
-
-        # Now compute what actual φdot_l and φdot_r will provide these.
-        _, _, _, φ_l, φ_r = s # x, y, θ not used
-        L_actual = self.actual_robot.L
-        v_r_actual = v_desired + (L_actual * ω_desired)
-        v_l_actual = v_desired - (L_actual * ω_desired)
-        R_l_actual = self.actual_robot.left_wheel.radius_at(φ_l)
-        R_r_actual = self.actual_robot.right_wheel.radius_at(φ_r)
-        φdot_l_actual = v_l_actual / R_l_actual
-        φdot_r_actual = v_r_actual / R_r_actual
-        u_actual = np.array([φdot_l_actual, φdot_r_actual])
-        return u_actual
-
+        assert False, 'To be implemented by child classes'
     #:translate()
 
-    '''
-    def execute_control_policy(self, t, Δt, u, s0 = np.array([0, 0, 0, 0, 0]), name=''):
-        sol = scipy.integrate.solve_ivp(dynamics,
-                                        (t[0], t[-1]), s0,
-                                        t_eval=t, max_step=kSimΔt,
-                                        args=(t, Δt, u, self))
-        assert sol.success
-        assert len(sol.t) == len(u) + 1
-        s = sol.y.T
-        assert len(s) == len(t)
-        return Trajectory(sol.t, s, u, name)
-    #:execute_control_policy
-
-    def execute_trajectory(self,
-                           orig_trajectory: Trajectory,
-                           new_trajectory_name) -> Trajectory:
-        # Execute the control policy in the original trajectory to
-        # generate a new one.
-        Δt = orig_trajectory.t[1] - orig_trajectory.t[0]
-        return self.execute_control_policy(orig_trajectory.t, Δt,
-                                           orig_trajectory.u,
-                                           orig_trajectory.s[0],
-                                           new_trajectory_name)
-    #:execute_trajectory()
-    '''
+    def ideal_body_dynamics(self,
+                            s: np.ndarray,
+                            v_ω_desired: np.ndarray) \
+        -> np.ndarray:
+        return self.ideal_robot.body_dynamics(s, v_ω_desired)
+    #:ideal_body_dynamics()
 
 #:KinematicController
 
+
 class KinematicallyControlledDDMR(DDMR):
-    def __init__(self, config_filename, ideal_robot, verbose=False) -> None:
+    def __init__(self,
+                 config_filename: str,
+                 controller: KinematicController,
+                 verbose:bool = False) \
+        -> None:
+
         super().__init__(config_filename)
-        self.name += '-kinCtl'
-        self.controller = KinematicController(ideal_robot, self)
-        self.verbose = False
+        self.controller = controller
+        self.name += f'-kinCtl-{controller.name}'
+        self.verbose = verbose
     #:
 
+    # Override
     def wheel_dynamics(self, s, φdots_ideal):
-        φdots_actual = self.controller.translate(φdots_ideal, s)
-        sdot_ideal = self.controller.ideal_robot.wheel_dynamics(s, φdots_ideal)
+        assert False, 'Should not be used'
+    #:
+
+    # Override
+    def body_dynamics(self, s, vω_desired):
+        φdots_actual = self.controller.translate(self, vω_desired, s)
         sdot_actual = super().wheel_dynamics(s, φdots_actual)
+        sdot_ideal = self.controller.ideal_body_dynamics(s, vω_desired)
         if self.verbose:
             x, y, θ, φ_l, φ_r = s
-            print(f'Dynamics @ ({x:0.3},{y:0.3};{θ:0.3}) | ({φ_l:0.3},{φ_r:0.3}):')
-            print(f'    φdots = {φdots_actual[0]:0.5}, {φdots_actual[1]:0.5} vs {φdots_ideal[0]:0.5}, {φdots_ideal[1]:0.5}')
+            print(f'{self.name} dynamics @ ({x:0.3},{y:0.3};{θ:0.3}) | ({φ_l:0.3},{φ_r:0.3}):')
+            print(f'    φdots = {φdots_actual[0]:0.5}, {φdots_actual[1]:0.5} vs {vω_desired[0]:0.5}, {vω_desired[1]:0.5}')
             print(f'    v = {np.hypot(sdot_actual[0], sdot_actual[1]):0.5} vs {np.hypot(sdot_ideal[0], sdot_ideal[1]):0.5}')
             print(f'    ω = {sdot_actual[2]:0.5} vs {sdot_ideal[2]:0.5}')
         #:if verbose
